693e35a722b324a3de3f3be17f37461219aa1dbd
Connor Wright <sc19c2sw@leeds.ac.uk>
2021-01-04 12:29:52 +0000

    added a post table which relates to the user table

--- application/app/models.py
+++ application/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -7,3 +8,10 @@ class User(db.Model):
     password = db.Column(db.String(128))
     dob = db.Column(db.DateTime)
     gender = db.Column(db.String(20))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500))
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
