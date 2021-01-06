9a1820c8958b548041a54c9cda648bdc269900c8
Connor Wright <sc19c2sw@leeds.ac.uk>
2021-01-06 13:52:16 +0000

    added Friends table to models.py and added related funtions for following/unfollowing a user

--- application/app/models.py
+++ application/app/models.py
@@ -8,6 +8,11 @@ from flask_login import UserMixin
 def load_user(id):
     return User.query.get(int(id))
 
+friendship = db.Table('friendships', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), index=True),
+    db.Column('friend_id', db.Integer, db.ForeignKey('user.id')))
+    #db.UniqueConstraint('user_id', 'friend_id', name='unique_friendships'))#prevents a user from following themself
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
@@ -16,12 +21,26 @@ class User(UserMixin, db.Model):
     dob = db.Column(db.DateTime)
     gender = db.Column(db.String(20))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    friends = db.relationship('User',
+                            secondary=friendship,
+                            primaryjoin=id==friendship.c.user_id,
+                            secondaryjoin=id==friendship.c.friend_id)
 
     def setPassword(self, plain_password):
         self.password = generate_password_hash(plain_password)
     
     def validatePassword(self, guess):
         return check_password_hash(self.password, guess)
+
+    def follow(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
+            #friend.friends.append(self)
+
+    def unfollow(self, friend):
+        if friend in self.friends:
+            self.friends.remove(friend)
+            #friend.friends.remove(self)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
